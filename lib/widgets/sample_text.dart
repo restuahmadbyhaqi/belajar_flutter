e7be1c7c9355932bc669a2f0a325aa6e2f3f35e5
unknown <restuahmad612@gmail.com>
2024-06-03 22:13:59 +0700

    calculator

--- lib/widgets/sample_text.dart
+++ lib/widgets/sample_text.dart
@@ -1,15 +1,46 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
 
-// class SampleText extends StatelessWidget {
-//   const SampleText({super.key});
+void main() {
+  runApp(const MyApp());
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold(
-//       appBar: AppBar(
-//         title: Text('belajar text widget'),
-//       ),
-//       body: Text('This is a sample text widget'),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Calculator'),
+        ),
+
+        body: Padding(
+          padding: EdgeInsets.all(18.0),
+          child: SizedBox(
+             width: double.infinity,
+             child: CalcWidget(),
+          ),
+         
+        )
+        
+      )
+
+    );
+  }
+}
+
+class CalcWidget extends StatefulWidget{
+  const CalcWidget({super.key});
+  @override
+  CalcWidgetState createState() => CalcWidgetState();
+}
+class CalcWidgetState extends State<CalcWidget>{
+  @override
+  Widget build(BuildContext context){
+    return const SimpleCalculator();
+  }
+}
+
