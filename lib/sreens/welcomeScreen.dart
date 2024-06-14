0f0c8c57c663d2ddb11b6afd9d71afcd48fa4dc3
unknown <restuahmad612@gmail.com>
2024-06-14 22:56:29 +0700

    welcome screen

--- lib/sreens/welcomeScreen.dart
+++ lib/sreens/welcomeScreen.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class WelcomeScreen extends StatelessWidget{
+class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
@@ -10,25 +11,80 @@ class WelcomeScreen extends StatelessWidget{
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Stack(
-          children:[
-            Container(
-              width:MediaQuery.of(context).size.width,
-              height:MediaQuery.of(context).size.height/ 1.6,
-              decoration:BoxDecoration(
-                color: Colors.white,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 74, 80, 239),
+                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(70)),
+                  ),
+                  child: Center(
+                    child: Image.asset("alquran.png", scale: 0.8),
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child:Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height /2.666,
+                padding: EdgeInsets.only(top: 40, bottom: 30),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 74, 80, 239),
+                ),
               ),
             ),
-            Container(
-              width:MediaQuery.of(context).size.width,
-              height:MediaQuery.of(context).size.height / 1.6,
-              decoration:BoxDecoration(
-                color: Color(0xFF674AEF),
-                borderRadius: 
-                  BorderRadius.only(bottomRight: Radius.circular(70)),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child:Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height /2.666,
+                padding: EdgeInsets.only(top: 40, bottom: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(70),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "IslamHub",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                        wordSpacing: 2,
+                      ),
+                      ),
+                      SizedBox(height: 15),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: Text(
+                          "Mari Belajar Agama, Insyaallah Berkah di Setiap Langkah",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black.withOpacity(0.6),
+                          ),
+                        ),
+                      )
+                  ],
+                )
               ),
-              child: Center(child: Image.asset("alquran.png", scale: 0.8,),),
             ),
-          ],),
+          ],
+        ),
       ),
     );
   }
